814a940eb1eb165acbe91588f4d7ccd572c4e2b1
Ignatio <ignatio.ferreira@gmail.com>
2022-12-08 17:14:22 +0200

    asdf

--- README.md
+++
@@ -1,6 +0,0 @@
-https://www.sohamkamani.com/java/spring-rest-http-server/
-https://www.sohamkamani.com/java/appengine/
-
-```
- mvn clean compile package && java -jar ./target/demo-0.0.1-SNAPSHOT.jar
-```
---
+++ src/main/java/com/example/poetrywebapp/PoetryWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.poetrywebapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PoetryWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoetryWebAppApplication.class, args);
+    }
+
+
+}
