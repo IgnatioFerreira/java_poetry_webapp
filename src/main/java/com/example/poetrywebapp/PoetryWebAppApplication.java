d98c041954cd71471bec3fe8e27fd825c44a92ca
Ignatio <ignatio.ferreira@gmail.com>
2022-12-08 20:16:05 +0200

    random-poem-mvp
    random-poem-mvp

--- src/main/java/com/example/poetrywebapp/PoetryWebAppApplication.java
+++ src/main/java/com/example/poetrywebapp/PoetryWebAppApplication.java
@@ -9,23 +9,37 @@ import java.util.Scanner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @SpringBootApplication
 @RestController
 public class PoetryWebAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) throws IOException {
         SpringApplication.run(PoetryWebAppApplication.class, args);
+
     }
 
-    @GetMapping("/lala")
-    public String simpleRequest() throws FileNotFoundException {
+    @GetMapping("/la")
+    public String simple_one() throws IOException {
+
         Random rand = new Random();
         String randomNumber = Integer.toString(rand.nextInt(0, 85));
-        File file = new File("src/poems/" + randomNumber + ".txt");
-        Scanner sc = new Scanner(file);
+
+        //https://howtodoinjava.com/java/io/read-file-from-resources-folder/
+        InputStream ioStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("data/poems/" + randomNumber + ".txt");
+
+        Scanner sc = new Scanner(ioStream);
         String content = sc.useDelimiter("\\A").next();
         sc.close();
 
         return content;
+
     }
+
 }
