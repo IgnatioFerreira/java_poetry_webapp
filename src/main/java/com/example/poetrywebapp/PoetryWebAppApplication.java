2bf25514104300d717c95cd1bffe11ebf8776af0
Ignatio <ignatio.ferreira@gmail.com>
2022-12-08 17:36:02 +0200

    lala

--- src/main/java/com/example/poetrywebapp/PoetryWebAppApplication.java
+++ src/main/java/com/example/poetrywebapp/PoetryWebAppApplication.java
@@ -1,14 +1,35 @@
 package com.example.poetrywebapp;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class PoetryWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PoetryWebAppApplication.class, args);
     }
 
+    @GetMapping("/lala")
+    public String simpleRequest() throws FileNotFoundException {
+        // Random rand = new Random();
+        // String randomNumber = Integer.toString(rand.nextInt(0, 85));
+        // File file = new File("src/poems/" + randomNumber + ".txt");
+        // Scanner sc = new Scanner(file);
+        // String content = sc.useDelimiter("\\A").next();
+        // sc.close();
 
+        // // In this case, we return the plain text response "ok"
+        // return content;
+
+        return "ok";
+    }
 }
+
